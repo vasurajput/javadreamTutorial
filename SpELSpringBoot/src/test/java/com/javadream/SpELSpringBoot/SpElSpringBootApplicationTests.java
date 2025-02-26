38bb71405ecebba0ee79cc8c248b363c615c25ac
vasurajput <vashurajput005@gmail.com>
2025-02-26 18:39:54 +0530

    Spring boot SpEL example

---
+++ SpELSpringBoot/src/test/java/com/javadream/SpELSpringBoot/SpElSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javadream.SpELSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpElSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
